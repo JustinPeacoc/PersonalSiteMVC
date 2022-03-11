0a6df12359edc77b38325e5a4dd7b71776ba6bf6
Justin Peacock <justinpeacock30@outlook.com>
2022-03-11 15:51:07 -0600

    Converted to MVC

---
+++ PersonalSiteMVC.UI/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PersonalSiteMVC.UI
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/_OriginalScripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/templateScripts").Include(
+                     "~/Content/assets/vendor/purecounter/purecounter.js", "~/Content/assets/vendor/aos/aos.js", "~/Content/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                     "~/Content/assets/vendor/glightbox/js/glightbox.min.js", "~/Content/assets/vendor/isotope-layout/isotope.pkgd.min.js", "~/Content/assets/vendor/swiper/swiper-bundle.min.js",
+                     "~/Content/assets/vendor/typed.js/typed.min.js", "~/Content/assets/vendor/waypoints/noframework.waypoints.js", "~/Content/assets/vendor/php-email-form/validate.js", "~/Content/assets/js/main.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/assets/vendor/aos/aos.css",
+                      "~/Content/assets/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/assets/vendor/bootstrap-icons/bootstrap-icons.css", "~/Content/assets/vendor/boxicons/css/boxicons.min.css", "~/Content/assets/vendor/glightbox/css/glightbox.min.css",
+                      "~/Content/assets/vendor/swiper/swiper-bundle.min.css", "~/Content/css/style.css"));
+            
+        }
+    }
+}
---
+++ PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using PersonalSiteMVC.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSiteMVC.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"You have received an email from <strong>{cvm.Name}</strong>. The email address given was <strong>{cvm.Email}</strong>.<br/><strong>The following message was sent:</strong> {cvm.Message}";
+
+            MailMessage mm = new MailMessage();
+            mm.From = new MailAddress("administrator@projectpeacock.net");
+            mm.To.Add(new MailAddress("justinpeacock30@outlook.com"));
+            mm.Subject = cvm.Subject;
+            mm.Body = body;
+
+            mm.IsBodyHtml = true;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient smtp = new SmtpClient("mail.projectpeacock.net");
+            NetworkCredential cred = new NetworkCredential("administrator@projectpeacock.net", "P@ssw0rd");
+
+            smtp.Credentials = cred;
+            smtp.Send(mm);
+            return Json(cvm);
+                
+        }
+
+        public PartialViewResult ContactConfirmation(string name, string email)
+        {
+            ViewBag.Name = name;
+            ViewBag.Email = email;
+            return PartialView("ContactConfirmation");
+        }
+    }
+}
